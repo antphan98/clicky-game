a460fc89dbdd7f134a93b83edf04c9d6b958f5eb
An Phan <antphan98@gmail.com>
2020-02-16 19:01:44 -0600

    set up basic files and added code into Card.js

---
+++ clicky-game/src/components/Card/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired
+};
+
+const Card = ({
+    id,
+    name,
+    image
+
+}) => (
+    <div className="card">
+      <div className="img-container">
+        <img alt={name} src={image} />
+      </div>
+    </div>
+  );
+
+Card.propTypes = propTypes;
+
+export default Card;
