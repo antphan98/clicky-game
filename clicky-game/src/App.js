92a6c62a860b938231ab5faf9ccf017e62243e14
An Phan <antphan98@gmail.com>
2020-02-18 21:23:23 -0600

    tried to load wrapper and card images but keep getting error

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import {
-Card,
-Wrapper,
-Header
-} from './components';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
 import cards from "./images.json";
 
-const App = () => {
-  return (
-    <Wrapper>
-        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+class App extends Component {
+
+  state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
+
+  render() {
+    return (
+      <Wrapper>
+
         {this.state.cards.map(card => (
           <Card
             clickCount={this.clickCount}
             id={card.id}
-            key={card.id}
             image={card.image}
           />
-        ))}
+
+        ))};
+        
       </Wrapper>
-  );
-}
+    );
+
+    }
+  }
+
+
 
 export default App;
